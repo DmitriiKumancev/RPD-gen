a78c05507290f747d8ac91d9a88d3d1a0e263a4b
Dmitrii Kumancev <dmitrijkumancev571@gmail.com>
2024-01-06 12:39:33 +0300

    add pluralize functions

--- document_gen.py
+++ document_gen.py
@@ -17,7 +17,31 @@ MONTHS = {
     12: 'Декабря'
 }
 
+def pluralize_hours(n):
+    if n % 10 == 1 and n % 100 != 11:
+        return f"{n} час"
+    elif n % 10 >= 2 and n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
+        return f"{n} часа"
+    else:
+        return f"{n} часов"
+
+def pluralize_semesters(n):
+    if not isinstance(n, int):
+        raise TypeError(f"Expected 'n' to be an int, got {type(n)} instead.")
+
+    if n == 1 or (n % 10 == 1 and n % 100 != 11):
+        return f"{n} семестр"
+    elif 2 <= n % 10 <= 4 and (n % 100 < 12 or n % 100 > 14):
+        return f"{n} семестра"
+    else:
+        return f"{n} семестров"
+
 def create_document(course_code, course_name, specialty_code, specialty_name, qualification, approval_year, semesters, total_hours):
+    if not isinstance(semesters, int):
+        raise TypeError(f"Expected 'semesters' to be an int, got {type(semesters)} instead.")
+    if not isinstance(total_hours, int):
+        raise TypeError(f"Expected 'total_hours' to be an int, got {type(total_hours)} instead.")
+
     tpl = DocxTemplate('template.docx')
     current_date = datetime.now()
     day = current_date.day
@@ -31,18 +55,16 @@ def create_document(course_code, course_name, specialty_code, specialty_name, qu
         'specialty_name': specialty_name,
         'qualification': qualification,
         'approval_year': approval_year,
-        'semesters': semesters,
-        'total_hours': total_hours,
+        'semesters': pluralize_semesters(semesters),
+        'total_hours': pluralize_hours(total_hours),
         'day': day,
         'month': month,
         'year': year
     }
 
     tpl.render(context)
-    
-    # Генерация уникального имени файла и сохранение его во временной директории
+
     with NamedTemporaryFile(delete=False, suffix='.docx', dir='/tmp') as tmp:
         tpl.save(tmp.name)
         unique_filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{course_code}_{course_name}.docx".replace(" ", "_")
-        # Возвращаем путь к временному файлу вместо имени файла
         return tmp.name, unique_filename
